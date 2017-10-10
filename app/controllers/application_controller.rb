b72fb8d0597f56f6211b9918039eee6e5b5e2c79
Rush Patel <rp3048@yahoo.com>
2017-10-10 17:39:13 +0000

    lab 4 part 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
+
 class ApplicationController < ActionController::Base
+  
   protect_from_forgery with: :exception
-   
+  
+  http_basic_authenticate_with name: "rus", password: "hi"
+  
+
   def hello
     render html: "hello, world!"
   end
